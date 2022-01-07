bbb8e6388279121fbf22d3f6c80bb6fbe7773d69
Don Heshanthaka <heshanthaka007@gmail.com>
2022-01-08 00:55:02 +0530

    code finalized with open addressing

--- main.c
+++ main.c
@@ -4,35 +4,35 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#define MAX_NAME 256 // maximum size of name allowed in bytes
+#define MAX_key 256 // maximum size of key allowed in bytes
 #define TABLE_SIZE 10
-#define DELETED_NODE (person*)(0xFFFFFFFFFFFFFFFFUL)
+#define DELETED_NODE (data*)(0xFFFFFFFFFFFFFFFFUL)
 
 
 // open addressing
 
-// creating the  struct of type person
+// creating the struct of type data
 typedef struct {
-    char name[MAX_NAME];
-    int age;
-} person;
+    char key[MAX_key];
+    int value;
+} data;
 
-person *hash_table[TABLE_SIZE]; // an array of pointers of type struct person [the array size is 10]
+data *hash_table[TABLE_SIZE]; // an array of pointers of type struct data [the array size is 10]
 
 // The function generates the hash value for the given input
-unsigned int hash(char *name){
-    int length = strnlen(name, MAX_NAME); // get the length of the input string
+unsigned int hash(char *key){
+    int length = strnlen(key, MAX_key); // get the length of the input string
     unsigned int hash_value = 0;
     for (int i = 0; i < length; i++){ // loop through every character of the string
-        hash_value += name[i];  // adding the value of each character to the hash_value
+        hash_value += key[i];  // adding the value of each character to the hash_value
         // the added hash_values are multiplied by the value of the character and mod by the size of the hash table
-        hash_value = (hash_value * name[i]) % TABLE_SIZE;
+        hash_value = (hash_value * key[i]) % TABLE_SIZE;
     }
     return hash_value;
 }
 
 // The function initialize the hash table
-void init_hash_table(){
+void initHashTable(){
     for (int i = 0; i < TABLE_SIZE; i++){   // the loop will run until the defined table size is reach
         hash_table[i] = NULL; // each index of the array is assigned by null
     }
@@ -40,21 +40,23 @@ void init_hash_table(){
 
 // This function prints the key, value pairs to the console
 void print_table(){
+    printf("     Element\t\tKey\t\tValue\n");
     for (int i=0; i < TABLE_SIZE; i++){ // the loop goes through the table
         if(hash_table[i] == NULL){  // if the element is empty
-            printf("\t%i\t---\n", i);
-        }else if (hash_table[i] == DELETED_NODE){ // if its a deleted node that was used previously
-            printf("\t%i\t<deleted>\n", i);
+            printf("\t%i\t\t---\n", i);
+        }else if (hash_table[i] == DELETED_NODE){ // if its a deleted location in the hash table that was used previously
+            printf("\t%i\t\t<deleted>\n", i);
         }else{ // prints the key, value pair
-            printf("\t%i\t%s\n",i, hash_table[i]->name);
+            printf("\t%i\t\t%s",i, hash_table[i]->key);
+            printf("\t\t%d\n", hash_table[i]->value);
         }
     }
 }
 
 // This function insert elements into the hash table
-bool hash_table_insert(person *p){
+bool insert(data *p){
     if(p == NULL) return NULL; // if the given element is null
-    int index = hash(p->name); // the hash value is returned by the hash_function for the given string of the element
+    int index = hash(p->key); // the hash value is returned by the hash_function for the given string of the element
     for (int i=0; i < TABLE_SIZE; i++){ // the loop will run upt to the size of the table
         /*
         since open addressing is used, the hash value is being incremented by 1 as the loop goes
@@ -72,8 +74,8 @@ bool hash_table_insert(person *p){
 }
 
 // this function find an element by its key value
-person *hash_table_lookup(char *name){
-    int index = hash(name); // generate the hash value based on the key
+data *search(char *key){
+    int index = hash(key); // generate the hash value based on the key
     for (int i = 0; i < TABLE_SIZE; i++){ // loop through the table
         int try = (index + i) % TABLE_SIZE; // the same algorithm is used as in the insert function
         // if the first hash value is null then its obvious that a key of similar hash value cannot be at another location, thereofore null is returned
@@ -83,7 +85,7 @@ person *hash_table_lookup(char *name){
         // if the selected hash_value index has been deleted, there is a possibility that more elements were added after, therefore the loop continues
         if(hash_table[try] == DELETED_NODE) continue;
         // if the selected key is matching then the pointer to that node is returned
-        if(strncmp(hash_table[index]->name, name, TABLE_SIZE) == 0){
+        if(strncmp(hash_table[index]->key, key, TABLE_SIZE) == 0){
             return hash_table[try];
         }
     }
@@ -91,8 +93,8 @@ person *hash_table_lookup(char *name){
 }
 
 // The function deletes an element based on the key value
-person *hash_table_delete(char *name){
-    int index = hash(name); // generate the hash value
+data *deleteItem(char *key){
+    int index = hash(key); // generate the hash value
     for (int  i = 0; i < TABLE_SIZE; i++){
         int try = (index + i) % TABLE_SIZE; // the same algorithm is used as in the insert function
         // if the first hash value is null then its obvious that a key of similar hash value cannot be at another location, thereofore null is returned
@@ -100,8 +102,8 @@ person *hash_table_delete(char *name){
         // if the selected hash_value index has been deleted, there is a possibility that more elements were added after, therefore the loop continues
         if (hash_table[try] == DELETED_NODE) continue;
         // if the selected key is matching then, the that node or index is assigned as a DELETED_NODE
-        if(strncmp(hash_table[try]->name, name, TABLE_SIZE) == 0){
-            person *tmp = hash_table[try];
+        if(strncmp(hash_table[try]->key, key, TABLE_SIZE) == 0){
+            data *tmp = hash_table[try];
             hash_table[try] = DELETED_NODE;
             return tmp;
         }
@@ -111,30 +113,26 @@ person *hash_table_delete(char *name){
 
 int main()
 {
-    init_hash_table();
+    initHashTable();
 
-    person jacob = {.name = "jacob", .age = 56};
-    person mike = {.name = "jacob", .age = 56};
-    person kate = {.name = "kate", .age = 26};
-    person mpho = {.name = "Mpho", .age = 36};
+    data laptop = {.key = "laptop", .value = 2000};
+    data phone = {.key = "phone", .value = 1000};
+    data tab = {.key = "tab", .value = 750};
+    data desktop = {.key = "desktop", .value = 2500};
 
-    hash_table_insert(&jacob);
-    hash_table_insert(&mike);
-    hash_table_insert(&kate);
-    hash_table_insert(&mpho);
+    insert(&laptop);
+    insert(&phone);
+    insert(&tab);
+    insert(&desktop);
 
-    hash_table_delete("Mpho");
-    hash_table_delete("jacob");
-    hash_table_delete("jacob");
+    deleteItem("laptop");
 
-    person susan = {.name = "jacob", .age = 56};
-    hash_table_insert(&susan);
 
-    person *lookupName = hash_table_lookup("Mpho");
-    if(lookupName == NULL){
-        printf("\nPerson not found!\n");
+    data *lookupkey = search("laptop");
+    if(lookupkey == NULL){
+        printf("\ndata not found!\n");
     }else{
-        printf("Found %s.\n", lookupName->name);
+        printf("Found %s.\n", lookupkey->key);
     }
 
     print_table();
